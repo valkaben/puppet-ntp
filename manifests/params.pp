680e47c40d8d1c49190cf571e7b66bb9dddba777
Benjamin R. Wong <benrwong@gmail.com>
2019-07-23 06:58:59 +0000

    first commit for ntp

---
+++ manifests/params.pp
@@ -0,0 +1,17 @@
+# a params class does not need parameters (no parenthesis'd list)
+class ntp::params {
+  $package_name       = 'ntp'
+  $package_ensure     = 'present'
+  $config_name        = 'ntp.conf'
+  $config_file_mode   = '0664'
+  $servers            = ['0.centos.pool.ntp.org', '1.centos.pool.ntp.org']
+  $service_ensure     = 'running'
+  $service_enable     = true
+  $service_hasrestart = true
+  $service_hasstatus  = true
+
+  $service_name = $facts['os']['family'] ? {
+    'Debian' => 'ntp', 
+    default  => 'ntpd', 
+  }
+}
